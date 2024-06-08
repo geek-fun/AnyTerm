0fe661f4d4aa8f6b17c986368ca32d1818ffc645
seven <zilisheng1996@gmail.com>
2024-06-08 20:38:26 +0800

    feat: generate pkg for appstre
    Signed-off-by: seven <zilisheng1996@gmail.com>

---
+++ scripts/build-pkg.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+set -o pipefail
+
+cd "$(dirname "$0")/.." || exit
+
+#source .env
+
+unset APPLE_SIGNING_IDENTITY
+
+unset APPLE_CERTIFICATE
+
+APP_NAME="AnyTerm"
+
+SIGN_APP="Apple Distribution: Lisheng Zi (Z44247ZSR9)"
+
+SIGN_INSTALL="3rd Party Mac Developer Installer: Lisheng Zi (Z44247ZSR9)"
+
+PROFILE="src-tauri/entitlements/AnyTerm_Distribution.provisionprofile"
+
+TARGET="universal-apple-darwin"
+
+
+npx tauri build --target "${TARGET}" --verbose
+
+# cargo tauri build --target "${target}" --verbose
+
+
+APP_PATH="src-tauri/target/${TARGET}/release/bundle/macos/${APP_NAME}.app"
+
+BUILD_NAME="src-tauri/target/${TARGET}/release/bundle/macos/${APP_NAME}.pkg"
+
+CP_DIR="src-tauri/target/${TARGET}/release/bundle/macos/${APP_NAME}.app/Contents/embedded.provisionprofile"
+
+ENTITLEMENTS="src-tauri/entitlements/${APP_NAME}.entitlements"
+
+
+
+cp "${PROFILE}" "${CP_DIR}"
+
+
+
+codesign --deep --force -s "${SIGN_APP}" --entitlements ${ENTITLEMENTS} "${APP_PATH}"
+
+
+
+productbuild --component "${APP_PATH}" /Applications/ --sign "${SIGN_INSTALL}" "${BUILD_NAME}"
